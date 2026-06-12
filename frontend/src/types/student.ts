6bbc6f6d98b249610838f9721ff9e523f6d63aa3
Ibiloyeolusegun <ibiloyeolusegun.1@gmail.com>
2026-06-12 16:57:15 +0100

    setup frontend architecture and login page

---
+++ frontend/src/types/student.ts
@@ -0,0 +1,13 @@
+export interface Student {
+  _id: string;
+  studentId: string;
+  name: string;
+  department: string;
+  level: string;
+  attendance: number;
+  assignmentScore: number;
+  testScore: number;
+  studyHours: number;
+  caScore: number;
+  prediction: string;
+}
